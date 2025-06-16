9b1e619a716901b42f287dec11fbd25b33450c16
YoshithMaddi <yoshithmy23@gmail.com>
2025-06-16 09:41:52 +0530

    tow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import Moviedetails from "./pages/Moviedetails";
 import Seatlayout from "./pages/Seatlayout";
+import Mybookings from "./pages/Mybookings";
+import Favorite from "./pages/Favorite";
+import { Toaster} from 'react-hot-toast'
+import Footer from "./components/Footer";
 
 const App = () => {
+  const isadminRoute = useLocation().pathname.startsWith('/admin')
   return (
     <>
-      <Navbar />
+    <Toaster/>
+      {!isadminRoute && <Navbar />}
       <Routes>
-        <Route path="='/"element={<Home/>} ></Route>
-        <Route path="='/movies"element={<Movies/>} ></Route>
-        <Route path="='/movies/:id"element={<Moviedetails/>} ></Route>
-        <Route path="='/movies/:id/:date"element={<Seatlayout/>} ></Route>
-        <Route path="='/my-bookings"element={<Seatlayout/>} ></Route>
+        <Route path="='/"element={<Home/>} />
+        <Route path="='/movies"element={<Movies/>} />
+        <Route path="='/movies/:id"element={<Moviedetails/>} />
+        <Route path="='/movies/:id/:date"element={<Seatlayout/>} />
+        <Route path="='/my-bookings"element={<Mybookings/>} />
+        <Route path="='/favorite"element={<Favorite/>} />
       </Routes>
+      {!isadminRoute && <Footer/>}
     </>
   );
 };
