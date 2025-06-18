e66d416b82c6f2d493131cee4bfb6925ccdbcf5d
YoshithMaddi <yoshithmy23@gmail.com>
2025-06-18 11:28:16 +0530

    uoto fav page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-
+import { assets } from "../assets/assets";
 const Footer = () => {
     return (
-        <footer className="px-6 pt-8 md:px-16 lg:px-36 w-full text-gray-300">
-            <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-10">
+        <footer className="px-6 md:px-16 lg:px-36 mt-40 w-full text-gray-300">
+            <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-14">
                 <div className="md:max-w-96">
-                    <img className="w-36 h-auto" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoWhite.svg" alt="logo" />
+                    <img className="w-36 h-auto" src={assets.logo_final} alt="logo" />
                     <p className="mt-6 text-sm">
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                     </p>
                     <div className="flex items-center gap-2 mt-4">
-                        <img src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/appDownload/googlePlayBtnBlack.svg" alt="google play" className="h-10 w-auto border border-white rounded" />
-                        <img src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/appDownload/appleStoreBtnBlack.svg" alt="app store" className="h-10 w-auto border border-white rounded" />
+                        <img src={assets.googlePlay} alt="google play" className="h-9 w-auto" />
+                        <img src={assets.appStore} alt="app store" className="h-9 w-auto" />
                     </div>
                 </div>
                 <div className="flex-1 flex items-start md:justify-end gap-20 md:gap-40">
                     <div>
-                        <h2 className="font-semibold mb-5">Company</h2>
+                        <h2 className="font-semibold mb-5">Mytickets</h2>
                         <ul className="text-sm space-y-2">
                             <li><a href="#">Home</a></li>
                             <li><a href="#">About us</a></li>
@@ -27,14 +27,14 @@ const Footer = () => {
                     <div>
                         <h2 className="font-semibold mb-5">Get in touch</h2>
                         <div className="text-sm space-y-2">
-                            <p>+1-234-567-890</p>
-                            <p>contact@example.com</p>
+                            <p>+91  9493648860</p>
+                            <p>yoshith12@gmail.com</p>
                         </div>
                     </div>
                 </div>
             </div>
             <p className="pt-4 text-center text-sm pb-5">
-                Copyright {new Date().getFullYear()} © PreBuiltUI. All Right Reserved.
+                Copyright {new Date().getFullYear()} © Mytickets. All Right Reserved.
             </p>
         </footer>
     )
