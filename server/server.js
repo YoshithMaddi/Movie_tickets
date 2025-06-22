9d2c256f262811c6338dfb644b92de741b5b9cc0
YoshithMaddi <yoshithmy23@gmail.com>
2025-06-22 15:44:07 +0530

    "new_full"

--- server/server.js
+++ server/server.js
@@ -1,23 +1,15 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
-import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
+import express from 'express';
+import cors from 'cors';
+import 'dotnet/config';
 
-
-const app =express();
-const port =3000;
-
-await connectDB()
+const app = express();
+const port=3000;
 
 //middleware
 app.use(express.json())
 app.use(cors())
-app.use(clerkMiddleware())
 
-// api routes
-app.get('/',(req,res)=>res.send('server is Live#'))
-app.use("/api/inngest", serve({ client: inngest, functions }));
-app.listen(port,()=> console.log(`server listing at http:localhost:${port}`))
+//api routes
+app.get('/',(req,res)=> res.send('Server is Live!'))
+
+app.listen(port,()=> console.log(`Server listening at https://localhost: ${port}`));
