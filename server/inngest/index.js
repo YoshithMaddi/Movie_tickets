92a83d3da5d2915e373751182ad087887e9ef785
YoshithMaddi <yoshithmy23@gmail.com>
2025-06-22 08:05:11 +0530

    Apply .gitignore rules

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,32 +2,29 @@ import { Inngest } from "inngest";
 import User  from "../models/User.js";
 export const inngest = new Inngest({ id: "Movie-ticket-booking" });
 
-
 // inngest func to save user data to a database
 const syncUserCreation=inngest.createFunction(
     {id:'sync-user-from-clerk'},
-    {event : 'clerk/user.created'},
+    {event:'clerk/user.created'},
     async({event})=>{
         const {id,first_name,last_name,email_addresses,image_url}=event.data
         const userdate={           
             _id:id,
-            email: email_addresses[0].email_address,
-            name: first_name+" "+last_name,
-            image: image_url,
+            email:email_addresses[0].email_address,
+            name:first_name+" "+last_name,
+            image:image_url,
 
         }
         await User.create(userdate)
     }
 )
 
-
 // inngest func to delete user from database
-
 const syncUserDeletion = inngest.createFunction(
     {id:'delete-user-with-clerk'},
-    {event : 'clerk/user.deleted'},
+    {event:'clerk/user.deleted'},
     async({event})=>{
-        const {id} = event.data
+        const {id}=event.data
         await User.findByIdAndDelete(id)
     }
 )
@@ -45,8 +42,6 @@ const syncUserUpdation=inngest.createFunction(
             image:image_url,
         }
         await User.findByIdAndDelete(id,userData);
-    }
-    
-) 
-
+    }  
+)
 export const functions = [syncUserCreation,syncUserDeletion,syncUserUpdation];
